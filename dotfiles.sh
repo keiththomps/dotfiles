18fa851330adda05aacde9c282295855596a5f4a
Keith Thompson <keith.thompson2690@gmail.com>
2012-05-12 20:47:55 -0400

    Added global gitignore file, & tweaked bash scripts

--- dotfiles.sh
+++ dotfiles.sh
@@ -9,11 +9,13 @@ create_links() {
   ln -s ${dir}/.bash_profile ~/.bash_profile
   ln -s ${dir}/.gitconfig ~/.gitconfig
   ln -s ${dir}/.githelpers ~/.githelpers
+  ln -s ${dir}/.gitignore_global ~/.gitignore_global
+  ln -s ${dir}/.jshintrc ~/.jshintrc
   ln -s ${dir}/.profile ~/.profile
   ln -s ${dir}/.tmux.conf ~/.tmux.conf
   ln -s ${dir}/.vim ~/.vim
+  rm -f ${dir}/.vim/.vim
   ln -s ${dir}/.vimrc ~/.vimrc
-  ln -s ${dir}/.jshintrc ~/.jshintrc
 }
 
 destroy_links() {
@@ -21,9 +23,10 @@ destroy_links() {
   rm -f ~/.bash_profile
   rm -f ~/.gitconfig
   rm -f ~/.githelpers
+  rm -f ~/.gitignore_global
+  rm -f ~/.jshintrc
   rm -f ~/.profile
   rm -f ~/.tmux.conf
   rm -rf ~/.vim
   rm -f ~/.vimrc
-  rm -f ~/.jshintrc
 }

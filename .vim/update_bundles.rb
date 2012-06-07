2bcf882226b9a19b0e9fd748725e837664678c2e
Keith Thompson <keith.thompson2690@gmail.com>
2012-06-06 22:04:21 -0400

    Updated aliases to add tmuxinator start alias, updated vim bundles to include endwise & vim-ruby. Also changed command-t's location as it's now hosted on github

--- .vim/update_bundles.rb
+++ .vim/update_bundles.rb
@@ -1,17 +1,19 @@
 #!/usr/bin/ruby
 git_bundles = [
   # General Purpose Plugins
-  "git://git.wincent.com/command-t.git",
+  "git://github.com/wincent/Command-T.git",
   "git://github.com/tpope/vim-pathogen.git",
   "git://github.com/tpope/vim-markdown.git",
   "git://github.com/tpope/vim-surround.git",
+  "git://github.com/tpope/vim-endwise.git",
   "git://github.com/scrooloose/syntastic.git",
   "git://github.com/tomtom/tcomment_vim.git",
-  # Python Plugins
-  "git://github.com/vim-scripts/pyflakes.vim.git",
   # Syntax highlighting
   "git://github.com/digitaltoad/vim-jade.git",
   "git://github.com/kchmck/vim-coffee-script.git",
+  "git://github.com/vim-ruby/vim-ruby.git",
+  # Python Plugins
+  "git://github.com/vim-scripts/pyflakes.vim.git",
 ]
 
 # Vim.org syntax scripts, also doesn't do anything

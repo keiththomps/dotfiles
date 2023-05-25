d42e843914d718966be7e48f2f62525434d3f177
Keith Thompson <keith.thompson@shopify.com>
2023-05-25 13:48:20 -0400

    Remap buffer format to get copy filename back

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -64,7 +64,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
   vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
   vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-  vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format { async = true } end, bufopts)
+  vim.keymap.set("n", "gf", function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()

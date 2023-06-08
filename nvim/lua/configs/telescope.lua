1bf1c9656d064bb54f48de64cebde77c3c92039f
Keith Thompson <keith.thompson@shopify.com>
2023-06-08 13:21:13 -0400

    Fix telescope buffer search keybind

--- nvim/lua/configs/telescope.lua
+++ nvim/lua/configs/telescope.lua
@@ -2,7 +2,7 @@
 vim.keymap.set("n", "<leader>lf", "<cmd>Telescope find_files<cr>", { noremap = true })
 vim.keymap.set("n", "<c-p>", "<cmd>Telescope find_files<cr>", { noremap = true })
 vim.keymap.set("n", "<leader>lg", "<cmd>Telescope live_grep<cr>", { noremap = true })
-vim.keymap.set("n", "<silent>;", "<cmd>Telescope buffers<cr>", { noremap = true })
+vim.keymap.set("n", ";", "<cmd>Telescope buffers<cr>", { silent = true, noremap = true })
 vim.keymap.set("n", "<leader>lh", "<cmd>Telescope help_tags<cr>", { noremap = true })
 
 local actions = require "telescope.actions"

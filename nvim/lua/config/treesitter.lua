e59eec6700e7471767e10d45d41f002e47d9c0f9
Keith Thompson <keith.thompson@shopify.com>
2025-04-25 19:50:01 -0400

    Clean up tree-sitter config

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,9 +1,7 @@
 require("nvim-treesitter.configs").setup {
-  ensure_installed = { "lua", "vim", "ruby", "html", "elixir", "eex", "heex", "javascript", "css" },
-  sync_install = false,
-  ignore_install = {},
   highlight = {
     enable = true,
     disable = {},
   },
+  auto_install = true,
 }
